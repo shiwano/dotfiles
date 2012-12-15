2a925453509330197b5049cc31d6ceefd8c94a1b
Shogo Iwano <shiwano@gmail.com>
2012-12-16 05:38:55 +0900

    Make a example dotfile enable

--- setup.rb
+++ setup.rb
@@ -6,55 +6,97 @@ include FileUtils::Verbose
 
 class String
   def expand
-    ret = Pathname.new(self).expand_path
-    ret.parent.mkpath unless ret.parent.exist?
-    ret
+    result = Pathname.new(self).expand_path
+    result.parent.mkpath unless result.parent.exist?
+    result
   end
 end
 
-def link(src, dst)
+def put_file(src, dst)
   src = Pathname.new(src).expand_path
   dst = Pathname.new(dst).expand_path
   dst.parent.mkpath unless dst.parent.exist?
-  remove_file dst if dst.directory? and dst.symlink?
-  ln_sf src.to_s, dst.to_s
+
+  if src.extname == '.example'
+    dst = Pathname.new dst.to_s.gsub(/\.example$/, '')
+    return puts "#{dst} already exists!" if dst.exist?
+    cp src.to_s, dst.to_s
+  else
+    remove_file dst if dst.directory? and dst.symlink?
+    ln_sf src.to_s, dst.to_s
+  end
 end
 
-cd '~'.expand
-'bin'.expand.mkpath unless 'bin'.expand.exist?
-system 'git clone git@github.com:shiwano/dotfiles.git dotfiles' unless 'dotfiles'.expand.exist?
-cd 'dotfiles'
+class Setup
+  def start
+    puts_separator 'git repo'
+    clone_repository
+    puts_separator 'bin'
+    set_up_bin
+    puts_separator 'dotfiles'
+    set_up_dotfiles
+    puts_separator 'vim plugins'
+    set_up_vim_plugins
+    puts_separator 'referencies'
+    download_ruby_ref
+    download_jquery_ref
+    download_javascript_ref
+  end
 
-Dir['bin/*'].each do |f|
-  link f, '~/bin'
+  def puts_separator(string)
+    puts "\n#{'#' * 25} [#{string}] #{'#' * 25}"
+  end
+
+  def clone_repository
+    cd '~'.expand
+    return puts 'dotfiles repository already exists!' if 'dotfiles'.expand.exist?
+    system 'git clone git@github.com:shiwano/dotfiles.git dotfiles'
+  end
+
+  def set_up_bin
+    cd '~'.expand
+    'bin'.expand.mkpath unless 'bin'.expand.exist?
+    Dir['bin/*'].each{ |path| put_file path, '~/bin' }
+  end
+
+  def set_up_dotfiles
+    cd '~/dotfiles'.expand
+    Dir['dot\.*'].each { |f| put_file f, '~/' + f.sub('dot', '') }
+  end
+
+  def set_up_vim_plugins
+    cd '~/dotfiles'.expand
+    return puts "vim plugins already exists!" if 'dot.vim/bundle/neobundle.vim'.expand.exist?
+    system 'git clone https://github.com/Shougo/neobundle.vim dot.vim/bundle/neobundle.vim'
+    system 'vim -c NeoBundleInstall -c quit'
+    cp 'settings/vimproc/post-merge'.expand, 'dot.vim/bundle/vimproc/.git/hooks/post-merge'.expand
+    chmod 0755, 'dot.vim/bundle/vimproc/.git/hooks/post-merge'.expand
+    system 'dot.vim/bundle/vimproc/.git/hooks/post-merge'
+  end
+
+  def download_ruby_ref
+    cd '~/dotfiles'.expand
+    return puts 'ruby ref already exists!' if 'refs/rubyrefm'.expand.exist?
+    system 'wget http://doc.okkez.net/archives/201107/ruby-refm-1.9.2-dynamic-20110729.zip'
+    system 'unzip ruby-refm-1.9.2-dynamic-20110729.zip'
+    rm 'ruby-refm-1.9.2-dynamic-20110729.zip'.expand
+    mv 'ruby-refm-1.9.2-dynamic-20110729'.expand, 'refs/rubyrefm'.expand
+  end
+
+  def download_jquery_ref
+    cd '~/dotfiles'.expand
+    return puts 'jquery ref already exists!' if '~/dotfiles/refs/jqapi'.expand.exist?
+    system 'wget http://jqapi.com/jqapi-latest.zip'
+    system 'unzip jqapi-latest.zip -d jqapi'
+    rm 'jqapi-latest.zip'.expand
+    mv 'jqapi'.expand, 'refs/jqapi'.expand
+  end
+
+  def download_javascript_ref
+    cd '~/dotfiles'.expand
+    return puts 'javascript ref already exists!' if '~/dotfiles/refs/jsref'.expand.exist?
+    system 'git clone https://github.com/tokuhirom/jsref.git refs/jsref'
+  end
 end
 
-Dir['dot\.*'].each do |f|
-  link f, '~/' + f.sub('dot', '')
-end
-
-unless 'dot.vim/bundle/neobundle.vim'.expand.exist?
-  system 'git clone https://github.com/Shougo/neobundle.vim dot.vim/bundle/neobundle.vim'
-  system 'vim -c NeoBundleInstall -c quit'
-  cp 'settings/vimproc/post-merge'.expand, 'dot.vim/bundle/vimproc/.git/hooks/post-merge'.expand
-  chmod 0755, 'dot.vim/bundle/vimproc/.git/hooks/post-merge'.expand
-  system 'dot.vim/bundle/vimproc/.git/hooks/post-merge'
-end
-
-unless 'refs/rubyrefm'.expand.exist?
-  system 'wget http://doc.okkez.net/archives/201107/ruby-refm-1.9.2-dynamic-20110729.zip'
-  system 'unzip ruby-refm-1.9.2-dynamic-20110729.zip'
-  rm 'ruby-refm-1.9.2-dynamic-20110729.zip'.expand
-  mv 'ruby-refm-1.9.2-dynamic-20110729'.expand, 'refs/rubyrefm'.expand
-end
-
-unless '~/dotfiles/refs/jqapi'.expand.exist?
-  system 'wget http://jqapi.com/jqapi-latest.zip'
-  system 'unzip jqapi-latest.zip -d jqapi'
-  rm 'jqapi-latest.zip'.expand
-  mv 'jqapi'.expand, 'refs/jqapi'.expand
-end
-
-unless '~/dotfiles/refs/jsref'.expand.exist?
-  system 'git clone https://github.com/tokuhirom/jsref.git refs/jsref'
-end
+Setup.new.start
