e6add6c6ff6f5573a9fea181697bc819ebc3550d
Shogo Iwano <shiwano@gmail.com>
2014-02-15 17:05:54 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 
-BIN_DIR=$HOME/bin
+UNAME=`uname`
+LOCAL_BIN_DIR=$HOME/bin
 DOTFILES_DIR=$HOME/dotfiles
 
-# clone repository
+topic() {
+  echo -en "\033[1;30m"
+  echo "$*"
+  echo -en "\033[0m"
+}
+
+topic 'Clone the repository'
+
 if [ -d $DOTFILES_DIR ]; then
-  echo 'dotfiles repository already exists!'
+  echo 'dotfiles repository already exists'
 else
   git clone --recursive git@github.com:shiwano/dotfiles.git $DOTFILES_DIR
 fi
 
-# setup bin
-if [ ! -d $BIN_DIR ]; then
-  mkdir $BIN_DIR
+topic 'Setup local bin files'
+
+if [ ! -d $LOCAL_BIN_DIR ]; then
+  mkdir $LOCAL_BIN_DIR
 fi
 
 for BIN in `find $DOTFILES_DIR/bin -type f -maxdepth 1`; do
-  echo 'Linking' $BIN '->' $BIN_DIR
-  ln -sf $BIN $BIN_DIR
+  echo 'Linking' $BIN '->' $LOCAL_BIN_DIR
+  ln -sf $BIN $LOCAL_BIN_DIR
 done
 
-# setup dotfiles
+topic 'Setup dotfiles'
+
 for DOTFILE in `find $DOTFILES_DIR -type f -maxdepth 1 -name 'dot.*' -regex '.*[^(example)]$'`; do
   DEST=$HOME/`basename $DOTFILE | sed -e 's/^dot\./\./'`
   echo 'Linking' $DOTFILE '->' $DEST
@@ -35,13 +45,37 @@ for DOTFILE in `find $DOTFILES_DIR -type f -maxdepth 1 -name 'dot.*.example'`; d
   fi
 done
 
-# setup vim plugins
+topic 'Setup Vim plugins'
+
 if [ -d $DOTFILES_DIR/dot.vim/bundle/neobundle.vim ]; then
-  echo 'vim plugins already exist!'
+  echo 'Vim plugins already exist'
 else
   if type vim > /dev/null 2>&1; then
     echo 'Installing Vim plugins'
     git clone --recursive git://github.com/Shougo/neobundle.vim $DOTFILES_DIR/dot.vim/bundle/neobundle.vim
     vim -c NeoBundleInstall -c quit
   fi
+fi
+
+topic 'Setup Homebrew'
+
+if [ $UNAME = "Darwin" ]; then
+  if type brew > /dev/null 2>&1; then
+    echo 'Homebrew is already installed'
+  else
+    echo 'Installing Homebrew'
+    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+    brew bundle $DOTFILES_DIR
+  fi
+else
+  echo 'This environment does not need homebrew'
+fi
+
+topic 'Setup anyenv'
+
+if type anyenv > /dev/null 2>&1; then
+  echo 'anyenv is already installed'
+else
+  echo 'Installing anyenv'
+  git clone https://github.com/riywo/anyenv $HOME/.anyenv
 fi
