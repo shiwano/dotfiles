812ed479e4bec6bfede1a19599795ff9ab8a657b
Shogo Iwano <shiwano@gmail.com>
2016-06-16 21:02:18 +0900

    go: Update go-tools.sh

--- go-tools.sh
+++ go-tools.sh
@@ -3,3 +3,4 @@ go get -u github.com/nsf/gocode
 go get -u github.com/golang/lint/golint
 go get -u golang.org/x/tools/cmd/goimports
 go get -u github.com/constabulary/gb/...
+go get -u github.com/rogpeppe/godef
