ba9ab57ad04223684bf3a793657c4da874f8f239
Shogo Iwano <shiwano@gmail.com>
2016-06-16 19:55:37 +0900

    go: Update go-tools.sh

--- go-tools.sh
+++ go-tools.sh
@@ -1,1 +1,5 @@
 go get -u github.com/nathany/looper
+go get -u github.com/nsf/gocode
+go get -u github.com/golang/lint/golint
+go get -u golang.org/x/tools/cmd/goimports
+go get -u github.com/constabulary/gb/...
