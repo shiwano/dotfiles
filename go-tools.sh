31ace0ed996c9062be323a0307e1ddfe41a51dc6
Shogo Iwano <shiwano@gmail.com>
2017-03-16 22:58:53 +0900

    go: Install dep and go-assets-builder

--- go-tools.sh
+++ go-tools.sh
@@ -4,3 +4,5 @@ go get -u github.com/motemen/ghq
 go get -u github.com/FiloSottile/gvt
 go get -u github.com/motemen/gore
 go get -u github.com/cweill/gotests/...
+go get -u github.com/jessevdk/go-assets-builder
+go get -u github.com/golang/dep
