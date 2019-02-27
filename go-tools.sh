25e1727256cf91add56d0bf03601009a1f69eaaa
Shogo Iwano <shiwano@gmail.com>
2019-02-27 23:13:21 +0900

    go: Update go-tools.sh

--- go-tools.sh
+++ go-tools.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-set -u
+set -exu
+
+GO111MODULE=off
 
 go get -u github.com/nathany/looper
 go get -u github.com/motemen/ghq
@@ -8,3 +10,5 @@ go get -u github.com/motemen/gore
 go get -u github.com/cweill/gotests/...
 go get -u github.com/jessevdk/go-assets-builder
 go get -u github.com/golang/dep/cmd/dep
+go get -u github.com/saibing/bingo
+go get -u github.com/go-delve/delve/cmd/dlv
