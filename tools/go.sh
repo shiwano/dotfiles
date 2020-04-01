039e89a693fb084c4ed22ac455dd54171fedc6c0
Shogo Iwano <shiwano@gmail.com>
2020-04-01 12:01:20 +0900

    Update go tools

--- tools/go.sh
+++ tools/go.sh
@@ -10,3 +10,4 @@ go get -u github.com/cweill/gotests/...
 go get -u github.com/go-delve/delve/cmd/dlv
 go get -u github.com/hashicorp/hcl2/cmd/hclfmt
 go get -u golang.org/x/tools/cmd/goimports
+go get -u github.com/pwaller/goimports-update-ignore
