f7cf5a115de06810d9a4b465bab4045c24cb55aa
Shogo Iwano <shiwano@gmail.com>
2020-07-25 12:20:24 +0900

    Add tools/go.sh

--- tools/go.sh
+++ tools/go.sh
@@ -4,11 +4,13 @@ set -exu
 
 GO111MODULE=off
 
+go get -u github.com/hashicorp/hcl2/cmd/hclfmt
+go get -u github.com/cweill/gotests/...
+go get -u github.com/davidrjenni/reftools/cmd/fillstruct
+go get -u golang.org/x/tools/cmd/goimports
+go get -u github.com/wvanlint/twf/cmd/twf
+go get -u github.com/go-delve/delve/cmd/dlv
+
 go get -u github.com/nathany/looper
 go get -u github.com/motemen/gore/cmd/gore
-go get -u github.com/cweill/gotests/...
-go get -u github.com/go-delve/delve/cmd/dlv
-go get -u github.com/hashicorp/hcl2/cmd/hclfmt
-go get -u golang.org/x/tools/cmd/goimports
 go get -u github.com/pwaller/goimports-update-ignore
-go get -u github.com/davidrjenni/reftools/cmd/fillstruct
