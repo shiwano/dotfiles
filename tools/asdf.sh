e5f0a4abc8593714a8cc6a2069426003eae6f595
Shogo Iwano <shiwano@gmail.com>
2021-03-17 18:01:52 +0900

    Add asdf configs

---
+++ tools/asdf.sh
@@ -0,0 +1,10 @@
+asdf plugin-add jq https://github.com/AZMCode/asdf-jq.git
+asdf plugin add kubectl
+asdf plugin add kustomize
+asdf plugin add kubesec
+asdf plugin add helm
+asdf plugin add helmfile
+asdf plugin add yq
+
+cd ~
+asdf install
