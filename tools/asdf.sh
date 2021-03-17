c5de1dc8eea2638ce31f90f13919dd5c1079dcb9
Shogo Iwano <shiwano@gmail.com>
2021-03-18 00:40:25 +0900

    Add direnv

--- tools/asdf.sh
+++ tools/asdf.sh
@@ -3,13 +3,14 @@
 set -eu
 cd "$(dirname $0)"
 
-asdf plugin-add jq https://github.com/AZMCode/asdf-jq.git || true
+asdf plugin add jq https://github.com/AZMCode/asdf-jq.git || true
 asdf plugin add kubectl || true
 asdf plugin add kustomize || true
 asdf plugin add kubesec || true
 asdf plugin add helm || true
 asdf plugin add helmfile || true
 asdf plugin add yq || true
-asdf plugin-add terraform || true
+asdf plugin add terraform || true
+asdf plugin add direnv || true
 
 asdf install
