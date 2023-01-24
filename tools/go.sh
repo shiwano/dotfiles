3acc91b9224d17300060f4172e2f4fa2bea34b5a
Shogo Iwano <shiwano@gmail.com>
2023-01-24 23:48:17 +0900

    Install github.com/aarzilli/gdlv

--- tools/go.sh
+++ tools/go.sh
@@ -6,6 +6,7 @@ go install github.com/hashicorp/hcl/v2/cmd/hclfmt@latest
 go install github.com/cweill/gotests/...@latest
 go install golang.org/x/tools/cmd/goimports@latest
 go install github.com/go-delve/delve/cmd/dlv@latest
+go install github.com/aarzilli/gdlv@latest
 go install golang.org/x/tools/gopls@latest
 go install github.com/x-motemen/gore/cmd/gore@latest
 go install github.com/pwaller/goimports-update-ignore@latest
