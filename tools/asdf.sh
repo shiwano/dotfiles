f6f105d3f1d0ea8c273c0476fd91ddc53b85b90a
Shogo Iwano <shiwano@gmail.com>
2025-02-28 10:45:14 +0900

    Add deno

--- tools/asdf.sh
+++ tools/asdf.sh
@@ -3,18 +3,19 @@
 set -eu
 cd "$(dirname $0)"
 
-asdf plugin add jq https://github.com/AZMCode/asdf-jq.git || true
-asdf plugin add kubectl || true
-asdf plugin add kustomize || true
-asdf plugin add kubesec || true
-asdf plugin add helm || true
-asdf plugin add helmfile || true
-asdf plugin add yq || true
-asdf plugin add terraform || true
-asdf plugin add direnv || true
-asdf plugin add ruby || true
-asdf plugin add flutter || true
-asdf plugin add nodejs || true
+asdf plugin-add jq https://github.com/AZMCode/asdf-jq.git || true
+asdf plugin-add kubectl || true
+asdf plugin-add kustomize || true
+asdf plugin-add kubesec || true
+asdf plugin-add helm || true
+asdf plugin-add helmfile || true
+asdf plugin-add yq || true
+asdf plugin-add terraform || true
+asdf plugin-add direnv || true
+asdf plugin-add ruby || true
+asdf plugin-add flutter || true
+asdf plugin-add nodejs || true
+asdf plugin-add deno https://github.com/asdf-community/asdf-deno.git || true
 
 asdf install
 asdf reshim
