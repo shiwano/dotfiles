d5976052ee0204c17706d0fad5b667ae6867415f
Shogo Iwano <shiwano@gmail.com>
2025-03-13 18:27:49 +0900

    Update init.lua

--- dot.config/nvim/init.lua
+++ dot.config/nvim/init.lua
@@ -1006,9 +1006,9 @@ end
 -------------------------------------------------------------------------------
 -- Indent
 -------------------------------------------------------------------------------
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.cindent = true
+vim.opt.autoindent = false
+vim.opt.smartindent = false
+vim.opt.cindent = false
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.tabstop = 2
