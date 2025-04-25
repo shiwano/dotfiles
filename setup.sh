3f6f1fa6390ca852528557b39c2d33f285731d97
Shogo Iwano <shiwano@gmail.com>
2025-04-25 10:53:34 +0900

    Update scripts

--- setup.sh
+++ setup.sh
@@ -3,9 +3,7 @@
 set -euo pipefail
 
 topic() {
-	echo -en "\033[1;34m"
-	echo "$*"
-	echo -en "\033[0m"
+	printf "\033[1;34m%s\033[0m\n" "$*"
 }
 
 main() {
@@ -13,64 +11,76 @@ main() {
 
 	topic 'Clone the repository'
 
-	if [ -d $dotfiles_dir ]; then
+	if [ -d "${dotfiles_dir}" ]; then
 		echo 'dotfiles repository already exists'
 	else
-		git clone git@github.com:shiwano/dotfiles.git $dotfiles_dir
+		git clone git@github.com:shiwano/dotfiles.git "${dotfiles_dir}"
 	fi
 
 	topic 'Setup bin directory'
 
 	local local_bin_dir=$HOME/bin
-	mkdir -p $local_bin_dir
+	mkdir -p "${local_bin_dir}"
 
-	for bin in $(find $dotfiles_dir/bin -type f -maxdepth 1 -mindepth 1 | grep -v '.DS_Store'); do
-		echo 'Linking' $bin '->' $local_bin_dir
-		ln -sf $bin $local_bin_dir
-	done
+	find "${dotfiles_dir}/bin" -type f -maxdepth 1 -mindepth 1 |
+		grep -v '.DS_Store' |
+		while IFS= read -r bin; do
+			echo 'Linking' "${bin}" '->' "${local_bin_dir}"
+			ln -sf "${bin}" "${local_bin_dir}"
+		done
 
 	topic 'Setup .config directory'
 
 	local local_dotconfig_dir=$HOME/.config
-	mkdir -p $local_dotconfig_dir
+	mkdir -p "${local_dotconfig_dir}"
 
-	for src in $(find $dotfiles_dir/dot.config -maxdepth 1 -mindepth 1 | grep -v '.DS_Store'); do
-		echo 'Linking' $src '->' $local_dotconfig_dir
-		ln -sf $src $local_dotconfig_dir
-	done
+	find "${dotfiles_dir}/dot.config" -maxdepth 1 -mindepth 1 |
+		grep -v '.DS_Store' |
+		while IFS= read -r src; do
+			echo 'Linking' "${src}" '->' "${local_dotconfig_dir}"
+			ln -sf "${src}" "${local_dotconfig_dir}"
+		done
 
 	topic 'Setup dotdirs'
 
-	for dotdir in $(find $dotfiles_dir -maxdepth 1 -mindepth 1 -type d -name 'dot.*' | grep -v 'example'); do
-		if [ $(basename $dotdir) = 'dot.config' ]; then
-			continue
-		fi
-		local dest="$HOME/$(basename $dotdir | sed -e 's/^dot\./\./')"
-		echo 'Linking' $dotdir '->' $dest
-		ln -sfn $dotdir $dest
-	done
+	find "$dotfiles_dir" -maxdepth 1 -mindepth 1 -type d -name 'dot.*' |
+		grep -v 'example' |
+		while IFS= read -r dotdir; do
+			if [ "$(basename "${dotdir}")" = 'dot.config' ]; then
+				continue
+			fi
+			local dest
+			dest="$HOME/$(basename "${dotdir}" | sed -e 's/^dot\./\./')"
+			echo 'Linking' "${dotdir}" '->' "${dest}"
+			ln -sfn "${dotdir}" "${dest}"
+		done
 
 	topic 'Setup dotfiles'
 
-	for dotfile in $(find "$dotfiles_dir" -maxdepth 1 -mindepth 1 -type f -name 'dot.*' | grep -v 'example'); do
-		local dest="$HOME/$(basename $dotfile | sed -e 's/^dot\./\./')"
-		echo 'Linking' $dotfile '->' $dest
-		ln -sfn $dotfile $dest
-	done
+	find "$dotfiles_dir" -maxdepth 1 -mindepth 1 -type f -name 'dot.*' |
+		grep -v 'example' |
+		while IFS= read -r dotfile; do
+			local dest
+			dest="$HOME/$(basename "${dotfile}" | sed -e 's/^dot\./\./')"
+			echo 'Linking' "${dotfile}" '->' "${dest}"
+			ln -sfn "${dotfile}" "${dest}"
+		done
 
-	for dotfile in $(find $dotfiles_dir -maxdepth 1 -mindepth 1 -type f -name 'dot.*.example'); do
-		local dest="$HOME/$(basename $dotfile | sed -e 's/^dot\./\./' | sed -e 's/\.example//')"
-		if [ ! -f $dest ]; then
-			echo 'Copying' $dotfile '->' $dest
-			cp $dotfile $dest
-		else
-			echo 'Already copied' $dotfile '->' $dest
-		fi
-	done
+	find "$dotfiles_dir" -maxdepth 1 -mindepth 1 -type f -name 'dot.*.example' |
+		while IFS= read -r dotfile; do
+			local dest
+			dest="$HOME/$(basename "${dotfile}" | sed -e 's/^dot\./\./' | sed -e 's/\.example//')"
+			if [ ! -f "${dest}" ]; then
+				echo 'Copying' "${dotfile}" '->' "${dest}"
+				cp "${dotfile}" "${dest}"
+			else
+				echo 'Already copied' "${dotfile}" '->' "${dest}"
+			fi
+		done
 
 	topic 'Setup bat themes'
 
-	if command -v bat 2>&1 >/dev/null; then
+	if command -v bat >/dev/null 2>&1; then
 		cd "$(bat --config-dir)/themes"
 		bat cache --build
 		cd -
